78a91fce0f5768b20e80f72a113a36fb049cf8a0
umarzp <mukthar.zp@gmail.com>
2024-10-27 13:49:33 +0530

    adding

--- db.php
+++ db.php
@@ -1,17 +1,12 @@
 <?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "onlineshop";
-
+$servername = "mysql";
+$username = "db";
+$password = "umar";
+$db = "php-app-db";
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
-
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-
 ?>
